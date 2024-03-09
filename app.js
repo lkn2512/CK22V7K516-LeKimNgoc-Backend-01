ffddce986e64211261887ef06793cfcf3e0aa089
lkn2512 <lkn25122001@gmail.com>
2024-03-09 19:09:21 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.json({message: "Welcome to contact book application."});
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
 });
 app.use("/api/contacts", contactsRouter);
 // handle 404 response
@@ -21,8 +21,8 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     // Middleware xử lý lỗi tập trung.
     // Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
-    return res.status(error.statusCode || 500).json({
-    message: error.message || "Internal Server Error",
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
     });
 });
 
